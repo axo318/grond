3040387856409462d62612ff72df709777f67722
Gregorio Fumagalli <greghi.fumagalli@gmail.com>
2024-03-14 17:42:19 +0100

    Test json object store endpoint with IDE

--- ms1/src/main/java/com/grond/Main.java
+++ ms1/src/main/java/com/grond/Main.java
@@ -1,45 +1,45 @@
 package com.grond;
 
-import com.grond.catalog.api.clients.BuildClient;
-import com.grond.catalog.api.clients.PipelineClient;
-import com.grond.catalog.api.objects.BuildCreateRequest;
-import com.grond.catalog.api.objects.BuildStatus;
-import com.grond.catalog.api.objects.Pipeline;
-import com.grond.catalog.api.objects.PipelineCreateRequest;
 import com.grond.common.dialog.ClientConfig;
-import java.util.Date;
-import java.util.List;
+import com.grond.object.store.api.clients.JsonStoreClient;
+import com.grond.object.store.api.objects.JsonKeyValue;
 
 public class Main {
 
     public static void main(String[] args) {
-        PipelineClient pipelineClient = new PipelineClient(
-            ClientConfig.builder().basePath("localhost:8080/catalog-service/api").build());
+        //        PipelineClient pipelineClient = new PipelineClient(ClientConfig.builder()
+        //                .basePath("localhost:8080/catalog-service/api")
+        //                .build());
+        //
+        //        BuildClient buildClient = new BuildClient(ClientConfig.builder()
+        //                .basePath("localhost:8080/catalog-service/api")
+        //                .build());
+        //
+        //        List<Pipeline> allPipelines = pipelineClient.getPipelines();
+        //        Pipeline pipeline;
+        //
+        //        if (allPipelines.isEmpty()) {
+        //            PipelineCreateRequest pipelineCreateRequest = PipelineCreateRequest.builder()
+        //                    .name("Gregorio's")
+        //                    .instructions("{}")
+        //                    .build();
+        //            pipeline = pipelineClient.createPipeline(pipelineCreateRequest);
+        //        } else {
+        //            pipeline = allPipelines.get(0);
+        //        }
+        //
+        //        BuildCreateRequest buildCreateRequest = BuildCreateRequest.builder()
+        //                .pipelineId(pipeline.getId())
+        //                .instructions("{}")
+        //                .status(BuildStatus.FAILED)
+        //                .startTime(new Date())
+        //                .build();
+        //        buildClient.createBuild(buildCreateRequest);
 
-        BuildClient buildClient = new BuildClient(
-            ClientConfig.builder().basePath("localhost:8080/catalog-service/api").build());
-
-        List<Pipeline> allPipelines = pipelineClient.getPipelines();
-        Pipeline pipeline;
-
-        if (allPipelines.isEmpty()) {
-            PipelineCreateRequest pipelineCreateRequest = PipelineCreateRequest.builder()
-                .name("Gregorio's")
-                .instructions("{}")
-                .build();
-            pipeline = pipelineClient.createPipeline(pipelineCreateRequest);
-        } else {
-            pipeline = allPipelines.get(0);
-        }
-
-        BuildCreateRequest buildCreateRequest = BuildCreateRequest.builder()
-            .pipelineId(pipeline.getId())
-            .instructions("{}")
-            .status(BuildStatus.FAILED)
-            .startTime(new Date())
-            .build();
-        buildClient.createBuild(buildCreateRequest);
+        JsonStoreClient jsonStoreClient = new JsonStoreClient(ClientConfig.builder()
+                .basePath("localhost:8081/object-store/api")
+                .build());
+        JsonKeyValue test = jsonStoreClient.getJSONKeyValue("123");
+        System.out.println(test);
     }
-
-
 }
